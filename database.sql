9dd4a1f76200b6f7289260499b8ec788d2d1294b
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2026-04-07 08:36:25 -0300

    Tarefa estudantes.

--- database.sql
+++ database.sql
@@ -4,3 +4,12 @@ create table luademel.pessoa(
     nome varchar(50)
 );
 insert into luademel.pessoa values (1,"tere");
+
+create table luademel.produto(
+  id INT PRIMARY KEY,
+  nome VARCHAR(300),
+  categoria VARCHAR(300),
+  preco DECIMAL(10,2),
+  data_criacao DATETIME,
+  data_modificacao DATETIME
+ );
--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,14 @@ interface IPessoa extends RowDataPacket {
     id: number,
     nome: string,
 }
+interface IProduto extends RowDataPacket {
+    id:number,
+    nome:string,
+    categoria:string,
+    preco:number,
+    data_criacao:Date,
+    data_modificacao:Date
+}
 
 const connection = mysql.createPool({
     host: 'localhost',
@@ -17,7 +25,7 @@ const connection = mysql.createPool({
 app.get("/pessoas", async (req, res) => {
     try {
         const [dados, campos] =
-            await connection.execute<IPessoa[]>('SELECT * FROM terepessoa')
+            await connection.execute<IPessoa[]>('SELECT * FROM pessoa')
         res.status(200).json(dados)
     } catch (err) {
         console.log(err)
@@ -68,18 +76,75 @@ app.post("/pessoas", async (req, res) => {
     }
 })
 
+app.post("/cadastro_produto", async (req, res) => {
+    const { id, nome, categoria, preco, data_criacao, data_modificacao } = req.body
+
+    if(id==''||id==null||nome==''||categoria==''||preco==''||data_criacao==''||data_modificacao==''){
+        return res.status(500).json({mensagem:"Dados enviados no formato errado, confira o JSON"})
+    }
+
+    try {
+        const [result] =
+            await connection
+                .execute<ResultSetHeader>('INSERT INTO produto VALUES (?,?,?,?,?,?)', 
+                                [id, nome, categoria, preco, data_criacao, data_modificacao])
+        if (result.affectedRows === 0)
+            return res.status(500).json({ mensagem: "Erro ao inserir!" })
+        return res.status(201).json({ mensagem: "Sucesso ao inserir!" })
+
+    } catch (err) {
+        console.log(err)
+        if (err instanceof Error && 'code' in err && err.code === 'ECONNREFUSED') {
+            return res.status(500).json({ mensagem: "ERRO: LIGUE O LARAGON e confira o usuário e senha da conexão" })
+        } else if (err instanceof Error && 'code' in err && err.code === 'ENOTFOUND') {
+            return res.status(500).json({ mensagem: "ERRO: Você digitou algo errado no host da conexão" })
+        } else if (err instanceof Error && 'code' in err && err.code === 'ER_BAD_DB_ERROR') {
+            return res.status(500).json({ mensagem: "ERRO: Confira o nome do banco de dados ou crie um banco com o nome que você passou na conexão" })
+        } else if (err instanceof Error && 'code' in err && err.code === 'ER_ACCESS_DENIED_ERROR') {
+            return res.status(500).json({ mensagem: "ERRO: Confira usuario e senha na conexão" })
+        } else if (err instanceof Error && 'code' in err && err.code === 'ER_PARSE_ERROR') {
+            return res.status(500).json({ mensagem: "ERRO: Você tem um erro na sua SQL, confira o Execute" })
+        } else if (err instanceof Error && 'code' in err && err.code === 'ER_NO_SUCH_TABLE') {
+            return res.status(500).json({ mensagem: "ERRO: Você digitou o nome da tabela errado, confira o Execute!" })
+        } else {
+            return res.status(500).json({ mensagem: "ERRO: Desconhecido!" })
+        }
+    }
+})
+
+app.get("/listar_produtos", async (req, res) => {
+    try {
+        const [dados, campos] =
+            await connection.execute<IProduto[]>('SELECT * FROM produto')
+        res.status(200).json(dados)
+    } catch (err) {
+        console.log(err)
+        if (err instanceof Error && 'code' in err && err.code === 'ECONNREFUSED') {
+            return res.status(500).json({ mensagem: "ERRO: LIGUE O LARAGON e confira o usuário e senha da conexão" })
+        } else if (err instanceof Error && 'code' in err && err.code === 'ENOTFOUND') {
+            return res.status(500).json({ mensagem: "ERRO: Você digitou algo errado no host da conexão" })
+        } else if (err instanceof Error && 'code' in err && err.code === 'ER_BAD_DB_ERROR') {
+            return res.status(500).json({ mensagem: "ERRO: Confira o nome do banco de dados ou crie um banco com o nome que você passou na conexão" })
+        } else if (err instanceof Error && 'code' in err && err.code === 'ER_ACCESS_DENIED_ERROR') {
+            return res.status(500).json({ mensagem: "ERRO: Confira usuario e senha na conexão" })
+        } else if (err instanceof Error && 'code' in err && err.code === 'ER_PARSE_ERROR') {
+            return res.status(500).json({ mensagem: "ERRO: Você tem um erro na sua SQL, confira o Execute" })
+        } else if (err instanceof Error && 'code' in err && err.code === 'ER_NO_SUCH_TABLE') {
+            return res.status(500).json({ mensagem: "ERRO: Você digitou o nome da tabela errado, confira o Execute!" })
+        } else {
+            return res.status(500).json({ mensagem: "ERRO: Desconhecido!" })
+        }
+    }
+})
+
+
+
 /**
  * No banco de dados 'luademel' crie uma nova tabela chamada produto
- * Na tabela produto, crie os seguintes atributos:
- * id INT
- * nome VARCHAR(300)
- * categoria VARCHAR(300)
- * preco DECIMAL(10,2)
- * data_criacao DATATIME
- * data_modificacao DATATIME
+ * Na tabela produto, crie os seguintes atributos 
  * 
- * Crie uma rota chamada `cadastro_produto` que eu possa enviar
- * um JSON para cadastrar um novo produto no banco de dados
+ *  id, nome, categoria, preco, data_criacao, data_modificacao
+ * 
  * 
  * Crie uma rota chamada `listar_produtos` que retorne todos
  * os produtos cadastrados no banco de dados
