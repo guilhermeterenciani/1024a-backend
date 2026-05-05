505bc2215d7a1661d7cce24f8cd86cfff439a61d
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2026-05-05 09:21:49 -0300

    Listando dados no terminal do chrome

--- src/index.ts
+++ src/index.ts
@@ -109,6 +109,7 @@ app.get("/listar_produtos_caros", async (req, res) => {
 //                      INNER JOIN pedidos p ON c.idclientes=p.clientes_idclientes
 app.get("/cliente_data_pedido", async (req, res) => {
     try {
+        console.log("Chamei a função no backend")
         const [dados, campos] =
             await connection.execute('SELECT nome,datapedido FROM clientes c INNER JOIN pedidos p ON c.idclientes=p.clientes_idclientes')
         res.status(200).json(dados)
